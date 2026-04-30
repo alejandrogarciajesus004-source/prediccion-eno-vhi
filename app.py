2596825528c2aac8ee786e6ca71ca7bc806b7245
alejandrogarciajesus004-source <alejandrogarciajesus004@gmail.com>
2026-04-30 13:36:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ st.divider()
 if st.button("CALCULATE RISK", type="primary", use_container_width=True):
     
     # 1. Diccionario con nombres y valores EXACTOS
-   input_dict = {
+input_dict = {
         'edad': edad, 
         'LAB_V_num_CHOL': chol, 
         'LAB_V_num_HDL': hdl, 
